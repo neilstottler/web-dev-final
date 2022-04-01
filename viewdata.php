75a84784ac8427317d2db6a2a60bb004a0a410c1
Neil S <44575567+neilstottler@users.noreply.github.com>
2022-04-01 10:22:26 -0400

    js graph test

--- viewdata.php
+++ viewdata.php
@@ -3,24 +3,26 @@
 require_once "config.php";
 
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+	header("location: login.php");
+	exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="css/nav-style.css">
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+	<link rel="stylesheet" href="css/nav-style.css">
 	<link rel="stylesheet" href="css/global.css">
+
 <body>
 
-<div class="Top">
+	<div class="Top">
 		<div class="rt-container">
 			<div class="col-rt-12">
 				<div class="Scriptcontent">
@@ -71,12 +73,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 		</div>
 	</div>
 
-    <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    <p>
-        <a href="reset-password.php">Reset Your Password</a>
-        <a href="logout.php">Sign Out of Your Account</a>
+	<h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+	<p>
+		<a href="reset-password.php">Reset Your Password</a>
+		<a href="logout.php">Sign Out of Your Account</a>
 		<a href="adddata.php">Add Data</a><br>
-    </p>
+	</p>
 
 	<table>
 		<tr>
@@ -86,46 +88,83 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 			<th>Location</th>
 			<th>Additional Notes</th>
 		</tr>
-	<?php 
+		<?php
 		//define variables
 		$userid = $itemname = $itemprice = $datebought = $location = $notes = "";
 		$itemname_err = $itemprice_err = $datebought_err = $location_err = $notes_err = "";
 		$username = $_SESSION["username"];
-	
+
 		//userid query
-		$query = "SELECT user_id FROM accounts where username = '". $username ."'";
+		$query = "SELECT user_id FROM accounts where username = '" . $username . "'";
 		$result_id = mysqli_query($link, $query);
 
 		//grab user ID from object thing and store it
 		if (mysqli_num_rows($result_id) > 0) {
 			// output data of each row
-			while($row = mysqli_fetch_assoc($result_id)) {
-			  $userid = $row["user_id"];
+			while ($row = mysqli_fetch_assoc($result_id)) {
+				$userid = $row["user_id"];
 			}
 		} else {
 			echo "0 results";
 		}
-		
+
 		//query DB for that specific
-		$query2 = "SELECT * FROM item_table WHERE user_id = ". $userid;
+		$query2 = "SELECT * FROM item_table WHERE user_id = " . $userid;
 		$result_items = mysqli_query($link, $query2);
 
 		//debugging shit
 		if (mysqli_num_rows($result_items) > 0) {
 			// output data of each row
-			while($row = mysqli_fetch_assoc($result_items)) {
-				echo('<tr>');
-				echo('<td>' . $row["item_name"] . '</td>'); 
-				echo('<td>' . $row["price"] . '</td>');
-				echo('<td>' . $row["date_bought"] . '</td>');
-				echo('<td>' . $row["location"] . '</td>');
-				echo('<td>' . $row["notes"] . '</td>');
-				echo('</tr>');
-			  }
+			while ($row = mysqli_fetch_assoc($result_items)) {
+				echo ('<tr>');
+				echo ('<td>' . $row["item_name"] . '</td>');
+				echo ('<td>' . $row["price"] . '</td>');
+				echo ('<td>' . $row["date_bought"] . '</td>');
+				echo ('<td>' . $row["location"] . '</td>');
+				echo ('<td>' . $row["notes"] . '</td>');
+				echo ('</tr>');
+			}
 		} else {
 			echo "0 results";
 		}
 		?>
 	</table>
+
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript">
+		google.charts.load('current', {
+			packages: ['corechart', 'line']
+		});
+		google.charts.setOnLoadCallback(drawBackgroundColor);
+
+		function drawBackgroundColor() {
+			var data = new google.visualization.DataTable();
+			data.addColumn('number', 'X');
+			data.addColumn('number', 'Dogs');
+
+			data.addRows([
+				[0, 0],
+				[1, 10]
+			]);
+
+			var options = {
+				hAxis: {
+					title: 'Time'
+				},
+				vAxis: {
+					title: 'Popularity'
+				},
+				backgroundColor: '#f1f8e9'
+			};
+
+			var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+			chart.draw(data, options);
+		}
+	</script>
+
+	<div id="chart_div"></div>
+
+
 </body>
+
 </html>
