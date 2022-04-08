2bb39b6fd15b2375db20553b93dc5ce65d10cdd2
manzim1atwit <56591022+manzim1atwit@users.noreply.github.com>
2022-04-08 10:25:43 -0400

    first pass at tpl

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include "topnav.php";
+
+?>
+
---
+++ topnav.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<div id =\"topbar\">";
+echo " <a href =\"index.html\"> Home </a>";
+echo " <a href =\"AboutUs.html\"> About Us </a>";
+echo "</div><br/>";
+?>
+
+
