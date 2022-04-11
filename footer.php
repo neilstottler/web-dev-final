7c08e80f92e4c9c005776cd1f10ba94956936bbf
RobertoGriffin <robertomgriffin@outlook.com>
2022-04-11 00:06:25 -0400

    Same changes
    changes to footer and about us

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
-<?php
-
-echo "<br/><div id =\"footer\">";
-echo "<footer>COMP4650 Web Development Project, Spring 2022</footer>";
-echo "</div>";
-
-?>
+<body>
+    <link rel="stylesheet" href="css/footer.css">
+</body>
+<footer>
+    <article class = "box">
+        
+    <p style = "color: black;" > COMP4650 Web Development Project. Spring 2022. </p>
+</div>
+</footer>
+</html>
