ae737f9093ff74a7d0f6e514b7ae173e744d63fa
RobertoGriffin <robertomgriffin@outlook.com>
2022-04-11 00:05:25 -0400

    About us and footer

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,10 @@
+<body>
+    <link rel="stylesheet" href="css/footer.css">
 </body>
 <footer>
-    COMP4650 Web Development Project. Spring 2022.
+    <article class = "box">
+        
+    <p style = "color: black;" > COMP4650 Web Development Project. Spring 2022. </p>
+</div>
 </footer>
 </html>
