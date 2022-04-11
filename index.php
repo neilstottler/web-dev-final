be05e85d74d87af307661b80ce61c6fd24786767
RobertoGriffin <robertomgriffin@outlook.com>
2022-04-11 00:17:35 -0400

    Fixed Index a little

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
-require "topnav.php";
-require "footer.php";
-?>
+<html>
+<head>
+	<link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
 
+<article class="center-block">
+
+    <br>
+    <img src="images/banking.png" class="title-image" alt="centered image" />
+
+    <p>Budgeting is an essential practice when learning to save money. Although we have found that there are many
+        ways to budget. We have compiled different examples of budgeting techniques for the user to become familiar
+        with the concept of budgeting.</p>
+</article>
+<body>
+</body>
+
+</html>
